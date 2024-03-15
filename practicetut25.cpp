4ef06be130aae26a59f8d0f7138d0b31b0393c6d
Zeeshan Choudhary <160936137+shanicoder007@users.noreply.github.com>
2024-03-16 02:55:40 +0500

    Add files via upload

---
+++ practicetut25.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class complex
+{
+
+    int a;
+    int b;
+
+public:
+    void setData(int v1, int v2)
+    {
+
+        a = v1;
+        b = v2;
+    }
+    void setDatabysum(complex o1, complex o2)
+    {
+
+        a = o1.a + o2.a;
+        b = o1.b + o2.b;
+    }
+    void printNumber()
+    {
+
+        cout << "Your complex number is :  " << a << "+" << b << "i" << endl;
+    }
+};
+
+int main()
+{
+
+    complex c1, c2, c3;
+
+    c1.setData(4, 5);
+    c1.printNumber();
+
+    c2.setData(5, 6);
+    c2.printNumber();
+
+    c3.setDatabysum(c1, c2);
+    c3.printNumber();
+
+    return 0;
+}
